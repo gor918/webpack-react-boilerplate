d31729c0db5945e7df44dd4967a7b9521fd49ddf
Gor <gordavtyan918@gmail.com>
2021-02-10 22:20:49 +0400

    removing unused code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,3 @@ window.addEventListener('load', () => {
         loadApp();
     });
 });
-
-if (process.env.NODE_ENV === 'production') {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').then(
-                (registration) => {
-                    console.log(`Service Worker registered! Scope: ${registration.scope}`);
-                },
-                (error) => {
-                    console.error(`Service Worker registration failed: ${error}`);
-                },
-            );
-        });
-    }
-}
